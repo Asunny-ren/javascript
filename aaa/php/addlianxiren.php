9096ab1d9d460bd4d8c5fcea8a45c5a3f96ea429
Asunny-ren <1398488165@qq.com>
2016-04-18 22:16:58 +0800

    啊

--- aaa/php/addlianxiren.php
+++
@@ -1,6 +0,0 @@
-<?php
-$mysqli= new mysqli("localhost","root","123456","test");
-$sql="INSERT INTO `contacts` (`name`,`phone`,`beizhu`) VALUES ('{$_GET['name']}','{$_GET['phone']}','{$_GET['beizhu']}')";
-$mysqli->query($sql);
-echo $mysqli->insert_id;
-?>
--- aaa/php/getlianxiren.php
+++
@@ -1,7 +0,0 @@
-<?php
-$mysqli= new mysqli("localhost","root","123456","test");
-$query="SELECT * FROM contacts ORDER by ID";
-$result=$mysqli->query($query);
-$row=$result->fetch_all(MYSQLI_ASSOC);
-echo json_encode($row);
-?>
